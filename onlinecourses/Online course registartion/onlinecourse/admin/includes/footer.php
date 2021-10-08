cb782b9da115fa591e699c9c03b9b89b6ea8f8de
Gravin3717 <86644962+Gravin3717@users.noreply.github.com>
2021-10-08 15:53:17 +0530

    Add files via upload

---
+++ onlinecourses/Online course registartion/onlinecourse/admin/includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    Online Course Registration | By : <a href="#" target="_blank">Gravin Patel and Neel Rami</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
